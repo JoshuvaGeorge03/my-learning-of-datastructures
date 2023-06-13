5cf5179572acf6504c19cf16f3d32df64ab1d01b
Joshuva George <joshuva.m@zohocorp.com>
2023-06-13 12:36:47 +0530

    implemented hash table set functionality

--- hash-table/HashTable.js
+++ hash-table/HashTable.js
@@ -1,21 +1,21 @@
 import { LinkedList } from "../linked-list/linked-list";
-import { getTypeOf } from '../utils/typechecker';
+import { getTypeOf } from "../utils/typechecker";
 
 const defaultHashTableSize = 32;
 
 export class HashTable {
   constructor(hashTableSize = defaultHashTableSize) {
+    this.hashTableSize = hashTableSize;
     this.hashTable = new Array(hashTableSize)
       .fill(null)
       .map(() => new LinkedList());
-      this.hashTableSize = hashTableSize;
-      this.keys = {};
+    this.keys = {};
   }
 
   hashCodeGenerate(codeString) {
-    let sum  = 0
-    for(let i = 0; i < codeString.length; i++) {
-      sum += codeString.charCodeAt(i)
+    let sum = 0;
+    for (let i = 0; i < codeString.length; i++) {
+      sum += codeString.charCodeAt(i);
     }
     return sum + codeString.length;
   }
@@ -25,45 +25,60 @@ export class HashTable {
   }
 
   allowedKeyTypes() {
-    return ['string'];
+    return ["string"];
   }
 
   get(key) {
-    if(!this.allowedKeyTypes().includes(getTypeOf(key))) {
-      throw 'not an supported type of keys, please use string as keys';
+    if (!this.allowedKeyTypes().includes(getTypeOf(key))) {
+      throw "not an supported type of keys, please use string as keys";
     }
 
+    if (!this.keys[key]) {
+      throw "no value for this key have been assigned before.";
+    }
     const hashCode = this.hashCodeGenerate(key);
-
-    if(!this.keys[hashCode]) {
-      throw 'no value for this key have been assigned before.'
-    }
-
     const index = this.convertHashCodeToIndex(hashCode);
     const hashTableLinkedList = this.hashTable[index];
 
-    const listNodeWhichContainsValueForTheSpecifiedKey = hashTableLinkedList.find((index, node) => node.data.key === key);
+    const listNodeWhichContainsValueForTheSpecifiedKey =
+      hashTableLinkedList.find((index, node) => node.data.key === key);
 
     return listNodeWhichContainsValueForTheSpecifiedKey.currentNode.data.value;
   }
 
   set(key, value) {
+    const hashCode = this.convertHashCodeToIndex(this.hashCodeGenerate(key));
+    this.keys[key] = hashCode;
 
+    const hashTableLinkedList = this.hashTable[hashCode];
+    if (hashTableLinkedList) {
+      const isCurrentKeyAlreadyPresentLinkedList = hashTableLinkedList.find(
+        (index, node) => node.data.key === key
+      );
+      if (isCurrentKeyAlreadyPresentLinkedList) {
+        isCurrentKeyAlreadyPresentLinkedList.currentNode.data = {
+          key,
+          value,
+        };
+      } else {
+        hashTableLinkedList.append({
+          key,
+          value,
+        });
+      }
+    } else {
+      hashTableLinkedList.append({
+        key: key,
+        value,
+      });
+    }
   }
 
-  delete(key) {
+  delete(key) {}
 
-  }
+  has(key) {}
 
-  has(key) {
-    
-  }
+  getKeys() {}
 
-  getKeys() {
-
-  }
-
-  getValues() {
-
-  }
+  getValues() {}
 }
