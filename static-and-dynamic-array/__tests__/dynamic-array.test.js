9e87befba6e515c9f2d3ab3e04b9eb5eb1a43c6c
Joshuva George <joshuva.m@zohocorp.com>
2023-07-06 10:51:10 +0530

    added partial testing for static array

---
+++ static-and-dynamic-array/__tests__/dynamic-array.test.js
@@ -0,0 +1,6 @@
+import { DynamicArray } from "../DynamicArray";
+
+
+describe('checking the functionality of dynamic array', () => {
+    test('test', () => undefined );
+})
--- static-and-dynamic-array/__tests__/static-array.test.js
+++ static-and-dynamic-array/__tests__/static-array.test.js
@@ -1,5 +1,18 @@
 import { StaticArray } from "../StaticArray";
 
 describe('ensure working of static array implemenation', () => {
-    
+
+    let myStaticArray = null;
+
+    beforeEach(() => {
+        myStaticArray = new StaticArray(10);
+    });
+
+    afterEach(() => {
+        myStaticArray = null;
+    })
+
+    test('whether the get item from static array works', () => {
+        expect(myStaticArray.getItem(0)).toBe(null);
+    });
 });
