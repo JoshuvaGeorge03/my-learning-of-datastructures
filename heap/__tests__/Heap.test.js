c8e7c71c51d6b3af277ca06e06db329a67a2dcff
Joshuva George <joshuva.m@zohocorp.com>
2023-08-02 10:27:33 +0530

    started working on heap DS working

--- heap/__tests__/Heap.test.js
+++ heap/__tests__/Heap.test.js
@@ -1,21 +1,13 @@
-import MinHeap from "../MinHeap"
-import Maxheap from '../MaxHeap';
 import BinaryHeap from "../Heap";
 
 describe('ensuring functional correctness of Heap', () => {
-    let testMinHeap;
-    let testMaxHeap;
     let binaryHeap;
 
     beforeEach(() => {
         binaryHeap = new BinaryHeap();
-        testMinHeap = new MinHeap();
-        testMaxHeap = new Maxheap();
     });
 
     afterEach(() => {
         binaryHeap = null;
-        testMaxHeap = null;
-        testMinHeap = null;
     });
 })
